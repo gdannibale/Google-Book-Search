410fce9abb1b17d0146aa88299080ff2f0693dd9
Gina Dannibale <ginadannibale@gmail.com>
2021-01-21 19:52:11 -0600

    change

--- google-book-search/client/src/components/list/container.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Container({ fluid, children }) {
-  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
-}
--- google-book-search/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const books = require("./googlebooks");
-
-// Book routes
-router.use("/books", books);
-
-module.exports = router;
